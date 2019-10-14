d420589723686678d2c0589d3a1b56870a7a3584
fitriwulandari19 <54261251+fitriwulandari19@users.noreply.github.com>
2019-10-14 21:18:04 +0800

    Add files via upload

---
+++ hitung.py
@@ -0,0 +1,13 @@
+import math
+
+def tambah(bil1, bil2 ):
+    return bil1 + bil2
+
+def kurang(bil1, bil2):
+    return bil1 - bil2
+
+def kali(bil1, bil2):
+    return bil1 * bil2
+
+def bagi(bil1, bil2):
+    return bil1 / bil2
